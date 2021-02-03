b55a8f909fed63f87098cba4d90ebe077d6410fc
shinz <shinzato704@gmail.com>
2021-02-03 14:56:11 +0900

    ch5-2-3

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+const fruits = ['リンゴ', 'モモ', 'バナナ']
 const ul = document.getElementById('fruit_list')
-const li = document.createElement('li')
-const text = document.createTextNode('リンゴ')
 
-li.appendChild(text)
-ul.appendChild(li)
+for(let cnt = 0; cnt < fruits.length; cnt++) {
+    const li = document.createElement('li')
+    const text = document.createTextNode(fruits[cnt])
+    li.appendChild(text)
+    ul.appendChild(li)
+}
