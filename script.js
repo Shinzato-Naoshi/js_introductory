de2a0264e28e548eb81e6dcd8dc80a823b4d9648
shinz <shinzato704@gmail.com>
2021-02-05 15:25:34 +0900

    ch5-4-3,time => setInterval 時刻の自動更新

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ console.log(new Date().getHours())
 console.log(new Date().getMinutes())
 console.log(new Date().getSeconds())
 
-const date = new Date()
-const hour = date.getHours()
-const min = date.getMinutes()
-const sec = date.getSeconds()
-
-time_el.innerText = hour + ':' + min + ':' + sec
+setInterval(function() {
+    const date = new Date()
+    const hour = date.getHours()
+    const min = date.getMinutes()
+    const sec = date.getSeconds()
+    time_el.innerText = hour + ':' + min + ':' + sec
+}, 1000)
