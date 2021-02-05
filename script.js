e4a92a3b2bc96c467e3b2b245ce334b6acc0c7bf
shinz <shinzato704@gmail.com>
2021-02-05 14:44:55 +0900

    ch5-4-2,time => new Data 現在時刻の取得と表示

---
+++ script.js
@@ -0,0 +1,12 @@
+const time_el = document.getElementById('time_el')
+console.log(new Date())
+console.log(new Date().getHours())
+console.log(new Date().getMinutes())
+console.log(new Date().getSeconds())
+
+const date = new Date()
+const hour = date.getHours()
+const min = date.getMinutes()
+const sec = date.getSeconds()
+
+time_el.innerText = hour + ':' + min + ':' + sec
