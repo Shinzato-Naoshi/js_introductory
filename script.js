9144694e6263953826ec65526136003ef2fbd6f4
shinz <shinzato704@gmail.com>
2021-02-12 14:39:09 +0900

    ch5-5-4, カウント処理を関数化

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ const action_btn = document.getElementById('action_btn')
 const counter = document.getElementById('counter')
 let count = 1
 
-action_btn.addEventListener('click', () => {
+function start_game() {
     setInterval(() => {
         count++
         counter.innerText = count
     }, 200)
-})
+}
+action_btn.addEventListener('click', start_game)
