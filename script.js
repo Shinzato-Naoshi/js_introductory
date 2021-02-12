632ae449e5cb2726508e207751478e399bfa1abc
shinz <shinzato704@gmail.com>
2021-02-12 14:57:33 +0900

    ch5-5-5, ボタンテキストの切り替え(stop)

--- script.js
+++ script.js
@@ -3,9 +3,14 @@ const counter = document.getElementById('counter')
 let count = 1
 
 function start_game() {
-    setInterval(() => {
-        count++
-        counter.innerText = count
-    }, 200)
+    const btn_text = action_btn.innerText
+
+    if (btn_text === 'start') {
+        setInterval(() => {
+            count++
+            counter.innerText = count
+        }, 200)
+        action_btn.innerText = 'stop'
+    }
 }
 action_btn.addEventListener('click', start_game)
