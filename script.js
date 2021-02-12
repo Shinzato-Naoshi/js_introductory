d411ecedb072450b48488697b22dbbb60b5b14ad
shinz <shinzato704@gmail.com>
2021-02-12 17:58:04 +0900

    ch5-5-7, 停止処理の実装

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 const action_btn = document.getElementById('action_btn')
 const counter = document.getElementById('counter')
-let count = 1
+let count = 0
+let interval_id
 
 function start_game() {
     const btn_text = action_btn.innerText
 
     if (btn_text === 'start') {
-        setInterval(() => {
+        interval_id = setInterval(() => {
             count++
             counter.innerText = count
         }, 200)
         action_btn.innerText = 'stop'
     } else if (btn_text === 'stop') {
-        console.log('This is stop')
+        clearInterval(interval_id)
+        action_btn.innerText = 'start'
+        count = 0
+        counter.innerText = count
     }
 }
 action_btn.addEventListener('click', start_game)
