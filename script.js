aa9e72abfb52daf5b396ef7eaf40f4bca6535df8
shinz <shinzato704@gmail.com>
2021-02-13 18:22:27 +0900

    ch5-5-8, 判定結果の表示

--- script.js
+++ script.js
@@ -15,7 +15,18 @@ function start_game() {
     } else if (btn_text === 'stop') {
         clearInterval(interval_id)
         action_btn.innerText = 'start'
-        count = 0
+        let li = document.createElement('li')
+        console.log(li)
+        if (count % 3 === 0) {
+            var text = document.createTextNode('カウント:' + count + '結果: 成功')
+        } else {
+            var text = document.createTextNode('カウント:' + count + '結果: 失敗')
+        }
+        li.appendChild(text)
+        console.log(text)
+        results.appendChild(li)
+        console.log(li)
+        count = 1
         counter.innerText = count
     }
 }
