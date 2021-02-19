9e9cc40086665f5f79ff9f7891255c6412e360fe
shinz <shinzato704@gmail.com>
2021-02-19 14:45:43 +0900

    jQuery ch7-4-3, submitButton 未入力時にボタンを無効

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // submitButtonを無効にする/常時
-// let $submit_btn = $('.submit_btn') //要素取得
+// const $submit_btn = $('.submit_btn') //要素取得
 
 // $submit_btn.prop('disabled', true) // buttonの無効化/常時
 
@@ -20,3 +20,41 @@
 // }
 
 
+// submitButtonを無効にする/未入力時のみ
+
+const $submit_btn = $('.submit_btn')
+const $text_field = $('.text_field')
+
+$submit_btn.prop('disabled', true)
+
+$text_field.on('keyup', () => {
+    const text_count = $text_field.val().length
+    if (text_count >= 1) {
+        $submit_btn.prop('disabled', false)
+    } else {
+        $submit_btn.prop('disabled', true)
+    }
+})
+
+
+// JavaScriptで書く
+
+// const submit_btn = document.getElementsByClassName('submit_btn')
+// const text_field = document.getElementsByClassName('text_field')
+// console.log(submit_btn)
+// console.log(text_field)
+// for (let i = 0; i < submit_btn.length; i++) {
+//     // const text_count = text_field[0].value.length // addEventListenerの外で定義したがaddEventListener内では認識されていないみたい,addEventListener内に書く
+//     submit_btn[i].disabled = true
+//     text_field[0].addEventListener('keyup', () => {
+//         const text_count = text_field[0].value.length // addEventListenerの関数内で定義
+//         if (text_count >= 1) {
+//             submit_btn[i].disabled = false
+//         }
+//         else {
+//             submit_btn[i].disabled = true
+//         }
+//         console.log(text_field[0].value.length)
+//         console.log(text_count)
+//     })
+// }
